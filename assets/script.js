6f69b60d74e1aa9b817854eebb2bba5abea7e59f
quikstart86 <thomastjack@gmail.com>
2024-01-14 18:19:29 +0000

    added in deafult cirty and added local storage as buttons to the left hand side

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-
-
+const defaultCity = `london`;
+const defaultUnits = `metric`;
 const apiKey = `e0fceb01398f5422268df38928be67d4`;
-const queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}units=metric`;
+const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&appid=${apiKey}&units=${defaultUnits}`;
 
 console.log(apiKey);
+
+// new
+let savedSearches = JSON.parse(localStorage.getItem(`savedSearches`)) || [];
 
 function displayTime() {
     $(`#TimeNow`).text(dayjs().format(`dddd, MMMM D, YYYY`))
@@ -13,7 +16,6 @@ function displayTime() {
 // update every second
 setInterval(displayTime, 1000)
 
-
 fetch(queryURL)
   .then(function (response) {
     // Calling .json() to access the json data stored inside the returned promise
@@ -21,12 +23,35 @@ fetch(queryURL)
   })
   // We store all of the retrieved data inside of an object called "data"
   .then(function (data) {
-
     // Log the queryURL
     console.log(queryURL);
 
-    // Log the resulting object
+    // Log the entire response object
     console.log(data);
 
-  
+    savedSearches.push(defaultCity);
+
+    localStorage.setItem(`savedSearches`, JSON.stringify(savedSearches));
+    displaySavedSearches();
   });
+
+  function displaySavedSearches() {
+    const $savedSearchList = $('#savedSearchList');
+    $savedSearchList.empty(); // Clear previous searches
+
+    savedSearches.forEach(function (city) {
+        $savedSearchList.append(`<button>${city}</button>`);
+    });
+}
+
+
+// Load a home screen with local weather set to london (4/5days)
+// able to search for other cities in a search bar
+// save searched cities to local storage
+// clear all search items
+
+
+  // Load a home screen with local weather set to london (4/5days)
+  // able to search for other cities in a search bar
+  // save seached cities to local storage
+  // clear all search items
